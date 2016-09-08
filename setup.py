c7250c6b4a72b7053cbe9b6093ac020789cea69a
aykol <muratahan@gmail.com>
2016-09-08 12:55:01 -0700

    restructuring and setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MaterialsCoord',
+    version='1.0.0',
+    packages=['materialscoord', 'materialscoord.external_src'],
+    url='https://github.com/aykol/MaterialsCoord',
+    license='A modified BSD license',
+    author='Muratahan Aykol',
+    author_email='maykol@lbl.gov',
+    description='Materials Coordination Infrastructure'
+)
