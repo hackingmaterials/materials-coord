c7974856b4151324fc87855460187add8a24aa4b
Alex Ganose <alexganose@googlemail.com>
2019-10-14 19:52:28 -0700

    Automatic plot size

--- materialscoord/plot.py
+++ materialscoord/plot.py
@@ -1,17 +1,20 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 from pandas import DataFrame
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_benchmark_scores(scores: DataFrame,
-                          structure_mapping: Optional[Dict[str, str]] = None,
-                          nn_method_mapping: Optional[Dict[str, str]] = None,
-                          figsize=(17, 10)
-                          ) -> plt:
+def plot_benchmark_scores(
+    scores: DataFrame,
+    structure_mapping: Optional[Dict[str, str]] = None,
+    nn_method_mapping: Optional[Dict[str, str]] = None,
+    figsize: Optional[Tuple[float, float]] = None,
+    vmax: Optional[float] = None,
+    round_dp: Optional[int] = 1,
+) -> plt:
     """
-    Plot MaterialsCoord benchmark scores.
+    Plot MaterialsCoord benchmark scores as a heatmap.
 
     Args:
         scores: The scores dataframe from `Benchmark.scores`.
@@ -23,10 +26,27 @@ def plot_benchmark_scores(scores: DataFrame,
             "BrunnerNN_reciprocal" to "BrunnerNN", nn_method_remapping should be::
 
                 {"BrunnerNN_reciprocal": "BrunnerNN"}
+        figsize: The figure size as a tuple of (width, height). If not provided this
+            will be automatically estimated based on the number of structures and
+            near neighbor methods.
+        vmax: The maximum value used for scaling the colorbar of the heatmap.
+        round_dp: Number of places to round the results to. If None, no rounding will
+            occur. Note that seaborn may automatically round the numbers anyway.
 
     Returns:
         A matplotlib pyplot object.
     """
+    if round_dp:
+        scores.round(round_dp)
+
+    if not figsize:
+        width = 4 + 1.5 * len(scores.columns)
+        height = 2 + 0.45 * len(scores.index)
+        figsize = (width, height)
+
+    if not vmax:
+        vmax = scores.iloc[:-1].values.max()
+
     structure_mapping = structure_mapping or {}
     nn_method_mapping = nn_method_mapping or {}
 
@@ -38,12 +58,12 @@ def plot_benchmark_scores(scores: DataFrame,
     for nn_label in scores.columns:
         nn_labels.append(nn_method_mapping.get(nn_label, nn_label))
 
-    sns.set(font='Helvetica')
+    sns.set(font="Helvetica")
     sns.set(font_scale=1.5)
-    sns.set_style({'axes.edgecolor': 'black', 'axes.linewidth': 1.3})
+    sns.set_style({"axes.edgecolor": "black", "axes.linewidth": 1.3})
 
     fig, ax = plt.subplots(figsize=figsize)
-    sns.heatmap(scores, annot=True, cmap="Blues", vmax=10)
+    sns.heatmap(scores, annot=True, cmap="Blues", vmax=vmax)
 
     ax.set_yticklabels(structure_labels)
     ax.set_xticklabels(nn_labels, rotation=60)
@@ -51,10 +71,10 @@ def plot_benchmark_scores(scores: DataFrame,
     # draw line above Total row and axis boundaries
     for _, spine in ax.spines.items():
         spine.set_visible(True)
-    ax.axhline(y=len(structure_labels)-1, color='k', linewidth=1.3)
+    ax.axhline(y=len(structure_labels) - 1, color="k", linewidth=1.3)
 
     cbar = ax.collections[0].colorbar
-    cbar.set_label('Benchmark score', labelpad=40, rotation=270)
-    cbar.outline.set(**{"edgecolor": 'k', "linewidth": 1.3})
+    cbar.set_label("Benchmark score", labelpad=40, rotation=270)
+    cbar.outline.set(**{"edgecolor": "k", "linewidth": 1.3})
 
     return plt
